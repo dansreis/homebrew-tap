7a9fe412b4f9ac1f8ed31d82852387f2a5d7b140
Daniel Reis <danielreis3195@gmail.com>
2026-07-13 00:09:33 +0000

    speclens 1.1.0

--- Casks/speclens.rb
+++ Casks/speclens.rb
@@ -1,6 +1,6 @@
 cask "speclens" do
-  version "1.0.3"
-  sha256 "334b9a4426bb1e00f3fdffae8995ff2d827e2d9c8af4636b241667788b354ec8"
+  version "1.1.0"
+  sha256 "6b1ec3fafa59a9cf1ef7812c6b3c08317c4b1fc30da21c53befb85fc29e8c9bf"
 
   url "https://github.com/dansreis/speclens/releases/download/v#{version}/SpecLens_#{version}_aarch64.dmg"
   name "SpecLens"
